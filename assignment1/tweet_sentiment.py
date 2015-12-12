53bc6c6460806f15ea63d5f32ee3142c97609207
Mike Kobit <mkobit@gmail.com>
2015-12-12 15:52:26 -0600

    Problem 2: Derive the sentiment of each tweet
    Problem 2 is the tweet_sentiment.py file
    The strategy here is that for each tweet:
    
    1. Remove @ mentions
    2. Remove # signs, perods (.), commas (,), explamation points (!), and question marks (?)
    3. encode in utf-8
    4. Iterate through term from the sentiment file (AFINN-111.txt). Check if contained in tweet
    5. If it is, add to the score
    
    The sentiment file is only 2.5k words, and the input file I am testing is only 20 MB,
    So this algorithm isn't that costly. There are also multi-word phrases
    in the sentiment file, which makes splitting a little hairier. One option would
    be to take the multi-word sentiments and check those explicitly while using
    some sort of splitting pattern, but I'll see if this is 'good enough' right now

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,35 @@
+import json
+import re
 import sys
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
-
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    scores = {}
+    with open(sys.argv[1]) as sentiments_file:
+        for line in sentiments_file:
+            term, score = line.split('\t')
+            scores[term] = int(score)
+
+    def compute_sentiment(text):
+        score = 0
+        normalized_text = re.sub('@[\w]{1,15}(\s|:)', ' ',  text.lower()) \
+            .replace('#', ' ') \
+            .replace('?', ' ') \
+            .replace('.', ' ') \
+            .replace(',', ' ') \
+            .replace('!', ' ') \
+            .encode('utf-8')
+        for term in scores:
+            if term in normalized_text:
+                score += scores[term]
+        return score
+
+    with open(sys.argv[2]) as tweet_file:
+        for line in tweet_file:
+            message = json.loads(line)
+            score = 0
+            if 'text' in message:
+                score = compute_sentiment(message['text'])
+            print(score)
 
 if __name__ == '__main__':
     main()
