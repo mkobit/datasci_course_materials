46f1a198a65df456209e211e369d1885d6b53fd9
Mike Kobit <mkobit@gmail.com>
2015-12-12 16:13:25 -0600

    Problem 3: Derive the sentiment of new terms
    'Derive the sentiment of new terms'. This problem has some scaffold in
    term_sentiment.py, and that is where the solution is done.
    
    I didn't read over the linked paper (probably should have), but my solution
    takes a simple approach.
    
    1. Use the same scoring strategy from the tweet_sentiment.py problem to calculate score
    2. Split the 'normalized' text
    3. Remove every word that is already in the sentiment file
    4. Calculate what percentage of the score this word represents
    5. Average the totals by key
    
    Once again, just using the firth approach that popped up. One problem I noticed
    is that most (if not all) of the scores are very low.

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,50 @@
+import collections
+import json
+import re
 import sys
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
-
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    scores = {}
+    with open(sys.argv[1]) as sentiments_file:
+        for line in sentiments_file:
+            term, score = line.split('\t')
+            scores[term] = int(score)
+
+    def compute_new_sentiments(text):
+        score = 0
+        # replace user names with space
+        normalized_text = re.sub('@[\w]{1,15}(\s|:)', ' ',  text.lower()) \
+            .replace('#', ' ') \
+            .replace('?', ' ') \
+            .replace('.', ' ') \
+            .replace(',', ' ') \
+            .replace('!', ' ') \
+            .encode('utf-8')
+        for term in scores:
+            if term in normalized_text:
+                score += scores[term]
+        new_sentiments = {}
+        if score:
+            split_normalized_text = normalized_text.split()
+            missing_term_counts = collections.Counter()
+            missing_terms = [term for term in split_normalized_text if term not in scores]
+            missing_term_counts.update(missing_terms)
+            for term, count in missing_term_counts.items():
+                new_sentiments[term] = float(score) * (float(count) / len(split_normalized_text))
+        return new_sentiments
+
+    with open(sys.argv[2]) as tweet_file:
+        new_term_values = collections.defaultdict(list)
+        for line in tweet_file:
+            message = json.loads(line)
+            score = 0
+            if 'text' in message:
+                new_sentiments = compute_new_sentiments(message['text'])
+                for key, value in new_sentiments.items():
+                    new_term_values[key].append(value)
+        for key, value in new_term_values.items():
+            average = sum(value) / len(value)
+            print('{}\t{}'.format(key, average))
 
 if __name__ == '__main__':
     main()
